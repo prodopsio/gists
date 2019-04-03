8f370b63772f1b1a304d8cd91e4b1bb671a8c847
Evgeny Zislis <evgeny.zislis@gmail.com>
2019-04-03 14:33:13 +0300

    add exponential retry for api error backoff

--- aws-r53-cleanup.sh
+++ aws-r53-cleanup.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script iterates over Route53 zone records, and DELETES the records
 # that point to an ELB in the chosen EC2 region that does not exist anymore.
 
 R53_HOSTED_ZONE_ID="ZXXXXXXXXXXXXX"
 
-TMPFILE=$(mktemp --tmpdir "$0.XXXXXXXXXXXX")
+TMPFILE="$(mktemp --tmpdir "$0.XXXXXXXXXXXX")"
 trap 'rm $TMPFILE' EXIT
 
 debug() { echo "$(date +%s%3N) $*" >&2; }
@@ -19,19 +19,29 @@ tm() {
     return $rc
 }
 
+# exponential retry
+# https://docs.aws.amazon.com/general/latest/gr/api-retries.html
+retry() {
+  retries=0
+  until "$@"; do
+    duration=$(( 2 ** retries ))
+    retries=$(( retries + 1))
+    debug "--> Retrying after ${duration}s"
+    sleep $duration
+  done
+}
+
 delete_record() {
     RECORD_SET="$(jq -c .ResourceRecordSets[0] "${TMPFILE}")"
     echo '{ "HostedZoneId": "'"$R53_HOSTED_ZONE_ID"'", "ChangeBatch": { "Changes": [ {"Action": "DELETE", "ResourceRecordSet": '"$RECORD_SET"'} ] } }' > "${TMPFILE}"
     debug "Deleting $RECORD_SET"
-    until tm aws route53 change-resource-record-sets --cli-input-json "file://$TMPFILE"
-    do debug 'Retrying'; sleep 1; done
+    retry tm aws route53 change-resource-record-sets --cli-input-json "file://$TMPFILE"
     rm "$TMPFILE"
 }
 
-until tm aws route53 list-resource-record-sets \
+retry tm aws route53 list-resource-record-sets \
     --hosted-zone-id "${R53_HOSTED_ZONE_ID}" \
     --max-items=1 --page-size=10 > "${TMPFILE}"
-do debug 'Retrying'; sleep 1; done
 NEXT_TOKEN="$(jq -r -c .NextToken "${TMPFILE}")"
 
 while [ -n "$NEXT_TOKEN" ]; do
@@ -46,11 +56,10 @@ while [ -n "$NEXT_TOKEN" ]; do
         fi
     fi
     debug "Finding next Route53 record in zone ${R53_HOSTED_ZONE_ID}..."
-    until tm aws route53 list-resource-record-sets \
+    retry tm aws route53 list-resource-record-sets \
         --hosted-zone-id "${R53_HOSTED_ZONE_ID}" \
         --max-items=1 --page-size=10 \
         --starting-token "${NEXT_TOKEN}" > "${TMPFILE}"
-    do debug 'Retrying'; sleep 1; done
     NEXT_TOKEN="$(jq -r -c .NextToken "${TMPFILE}")"
     ALIAS_TARGET="$(jq -r -c .ResourceRecordSets[0].AliasTarget.DNSName "${TMPFILE}")"
 done
